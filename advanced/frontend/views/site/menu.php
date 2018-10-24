9ad9471bf3bc22ad0e42956b9e5534f8df1292b2
2170330 <2170330@users.noreply.github.com>
2018-10-24 17:20:43 +0100

    a
    a

--- advanced/frontend/views/site/menu.php
+++ advanced/frontend/views/site/menu.php
@@ -6,9 +6,31 @@ use yii\helpers\Html;
 
 $this->title = 'Menu';
 $this->params['breadcrumbs'][] = $this->title;
+
+/*public function __construct(){
+    parent::__construct();
+    $this->load->helper('url');
+}*/
 ?>
-<div class="site-menu">
 
 
+<div class="row">
+    <div class="site-menu-1 col-lg-3">
+        <a class="btn-reservar col-spaced" href="http://www.yiiframework.com">Reserve já</a>
+    </div>
+    <div class="site-menu-2 col-lg-9">
+        <p class="titulo-menu"> Menus </p>
+        <p class="line"> _____</p>
 
+        <div class="site-menu-2-1 col-lg-4">
+            <img class="site-menu-2-image" src="../../web/img/mistura_comida.jpg">
+        </div>
+        <div class="site-menu-2-2 col-lg-4">
+            <img class="site-menu-2-image" src="../../web/img/mistura_comida.jpg">
+        </div>
+        <div class="site-menu-2-2 col-lg-4">
+            <img class="site-menu-2-image" src="../../web/img/mistura_comida.jpg">
+        </div>
+
+    </div>
 </div>
