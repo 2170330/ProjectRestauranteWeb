d34238af7cc2b38fe273f00e59552e136068f6d3
2170330 <2170330@users.noreply.github.com>
2019-01-03 09:23:13 +0000

    Revert "Merge branch 'master' of https://github.com/2170330/ProjectRestauranteWeb"
    This reverts commit 9208df30afedc0c66b595da65f422a196925a9df, reversing
    changes made to 237808decd01acbeaf27c26369d3484b6da5a551.

---
+++ ProjectAndroidRestaurante/app/src/main/java/pt/ipleiria/estg/dei/amsi/projectandroidrestaurante/MenuPrincipalActivity.java
@@ -0,0 +1,14 @@
+package pt.ipleiria.estg.dei.amsi.projectandroidrestaurante;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+    }
+}
