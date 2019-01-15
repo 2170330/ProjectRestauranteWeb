1ecc6fcd3b73cada7bac825f8f21cf07a390ba24
Dinis Roxo <2170353@my.ipleiria.pt>
2019-01-15 12:19:14 +0000

    s

--- AndroidPR/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v7/recyclerview/R.java
+++ AndroidPR/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v7/recyclerview/R.java
@@ -110,34 +110,98 @@ public final class R {
         public static final int async = 0x7f090021;
         public static final int blocking = 0x7f090025;
         public static final int bottom = 0x7f090026;
-        public static final int chronometer = 0x7f090033;
-        public static final int end = 0x7f09004d;
-        public static final int forever = 0x7f09005d;
-        public static final int icon = 0x7f090066;
-        public static final int icon_group = 0x7f090067;
-        public static final int info = 0x7f090083;
-        public static final int italic = 0x7f090085;
-        public static final int item_touch_helper_previous_elevation = 0x7f090087;
-        public static final int left = 0x7f09008d;
-        public static final int line1 = 0x7f09008e;
-        public static final int line3 = 0x7f09008f;
-        public static final int none = 0x7f0900ac;
-        public static final int normal = 0x7f0900ad;
-        public static final int notification_background = 0x7f0900ae;
-        public static final int notification_main_column = 0x7f0900af;
-        public static final int notification_main_column_container = 0x7f0900b0;
-        public static final int right = 0x7f0900bf;
-        public static final int right_icon = 0x7f0900c0;
-        public static final int right_side = 0x7f0900c1;
-        public static final int start = 0x7f0900ea;
-        public static final int tag_transition_group = 0x7f0900f0;
-        public static final int tag_unhandled_key_event_manager = 0x7f0900f1;
-        public static final int tag_unhandled_key_listeners = 0x7f0900f2;
-        public static final int text = 0x7f0900f3;
-        public static final int text2 = 0x7f0900f4;
-        public static final int time = 0x7f0900ff;
-        public static final int title = 0x7f090100;
-        public static final int top = 0x7f090104;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        public static final int chronometer = 0x7f090034;
+        public static final int end = 0x7f09004e;
+        public static final int forever = 0x7f09005e;
+        public static final int icon = 0x7f090067;
+        public static final int icon_group = 0x7f090068;
+        public static final int info = 0x7f090086;
+        public static final int italic = 0x7f090088;
+        public static final int item_touch_helper_previous_elevation = 0x7f090089;
+        public static final int left = 0x7f09008f;
+        public static final int line1 = 0x7f090090;
+        public static final int line3 = 0x7f090091;
+        public static final int none = 0x7f0900ae;
+        public static final int normal = 0x7f0900af;
+        public static final int notification_background = 0x7f0900b0;
+        public static final int notification_main_column = 0x7f0900b1;
+        public static final int notification_main_column_container = 0x7f0900b2;
+        public static final int right = 0x7f0900c1;
+        public static final int right_icon = 0x7f0900c2;
+        public static final int right_side = 0x7f0900c3;
+=======
+=======
+>>>>>>> parent of 67b85b8... android
+=======
+>>>>>>> parent of 67b85b8... android
+=======
+>>>>>>> parent of 67b85b8... android
+        public static final int chronometer = 0x7f090035;
+        public static final int end = 0x7f09004f;
+        public static final int forever = 0x7f09005f;
+        public static final int icon = 0x7f090069;
+        public static final int icon_group = 0x7f09006a;
+        public static final int info = 0x7f090087;
+        public static final int italic = 0x7f090089;
+        public static final int item_touch_helper_previous_elevation = 0x7f09008b;
+        public static final int left = 0x7f090091;
+        public static final int line1 = 0x7f090092;
+        public static final int line3 = 0x7f090093;
+        public static final int none = 0x7f0900ad;
+        public static final int normal = 0x7f0900ae;
+        public static final int notification_background = 0x7f0900af;
+        public static final int notification_main_column = 0x7f0900b0;
+        public static final int notification_main_column_container = 0x7f0900b1;
+        public static final int right = 0x7f0900c0;
+        public static final int right_icon = 0x7f0900c1;
+        public static final int right_side = 0x7f0900c2;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 67b85b8... android
+=======
+>>>>>>> parent of 67b85b8... android
+=======
+>>>>>>> parent of 67b85b8... android
+=======
+>>>>>>> parent of 67b85b8... android
+        public static final int start = 0x7f0900ed;
+        public static final int tag_transition_group = 0x7f0900f3;
+        public static final int tag_unhandled_key_event_manager = 0x7f0900f4;
+        public static final int tag_unhandled_key_listeners = 0x7f0900f5;
+        public static final int text = 0x7f0900f6;
+        public static final int text2 = 0x7f0900f7;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        public static final int time = 0x7f090103;
+        public static final int title = 0x7f090104;
+        public static final int top = 0x7f090108;
+=======
+        public static final int time = 0x7f090102;
+        public static final int title = 0x7f090103;
+        public static final int top = 0x7f090107;
+>>>>>>> parent of 67b85b8... android
+=======
+        public static final int time = 0x7f090102;
+        public static final int title = 0x7f090103;
+        public static final int top = 0x7f090107;
+>>>>>>> parent of 67b85b8... android
+=======
+        public static final int time = 0x7f090102;
+        public static final int title = 0x7f090103;
+        public static final int top = 0x7f090107;
+>>>>>>> parent of 67b85b8... android
+=======
+        public static final int time = 0x7f090102;
+        public static final int title = 0x7f090103;
+        public static final int top = 0x7f090107;
+>>>>>>> parent of 67b85b8... android
     }
     public static final class integer {
         private integer() {}
@@ -147,12 +211,12 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0c003e;
-        public static final int notification_action_tombstone = 0x7f0c003f;
-        public static final int notification_template_custom_big = 0x7f0c0046;
-        public static final int notification_template_icon_group = 0x7f0c0047;
-        public static final int notification_template_part_chronometer = 0x7f0c004b;
-        public static final int notification_template_part_time = 0x7f0c004c;
+        public static final int notification_action = 0x7f0c003f;
+        public static final int notification_action_tombstone = 0x7f0c0040;
+        public static final int notification_template_custom_big = 0x7f0c0047;
+        public static final int notification_template_icon_group = 0x7f0c0048;
+        public static final int notification_template_part_chronometer = 0x7f0c004c;
+        public static final int notification_template_part_time = 0x7f0c004d;
     }
     public static final class string {
         private string() {}
