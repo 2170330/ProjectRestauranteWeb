4ca506ffbb2d91e49ee49b458d13e54299d1535b
Dinis Roxo <2170353@my.ipleiria.pt>
2019-01-25 01:17:22 +0000

    hjg

---
+++ ProjectAndroidRestaurante/app/src/main/java/pt/ipleiria/estg/dei/amsi/projectandroidrestaurante/NumMesaActivity.java
@@ -0,0 +1,65 @@
+<<<<<<< HEAD
+package pt.ipleiria.estg.dei.amsi.projectandroidrestaurante;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class NumMesaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] NumMesa = {"1", "2", "3"};
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_num_mesa);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinnerNumMesa);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void Inicio_Sessao(View view) {
+        Intent intentIS = new Intent(this, StartActivity.class);
+        startActivity(intentIS);
+    }
+}
+=======
+package pt.ipleiria.estg.dei.amsi.projectandroidrestaurante;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NumMesaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_num_mesa);
+    }
+}
+>>>>>>> ba7b66e4b25c3e1fedfe7dcca4f02f5811505159
---
+++ ProjectAndroidRestaurante/app/src/main/java/pt/ipleiria/estg/dei/amsi/projectandroidrestaurante/StartActivity.java
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+package pt.ipleiria.estg.dei.amsi.projectandroidrestaurante;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class StartActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+    }
+
+    public void Register(View view) {
+        Intent intentR = new Intent(this, RegisterActivity.class);
+        startActivity(intentR);
+    }
+}
+=======
+package pt.ipleiria.estg.dei.amsi.projectandroidrestaurante;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
+>>>>>>> ba7b66e4b25c3e1fedfe7dcca4f02f5811505159
